d3b276d42e4135ca724776e5d975714d4725438b
Aaron Moore <aaron@atamisk.net>
2023-06-19 18:16:33 -0400

    Adding comments and light work on initial data model.

--- 04_input_handling/input_handling.c
+++ 04_input_handling/input_handling.c
@@ -2,14 +2,28 @@
 #include <gui/scene_manager.h>
 #include <gui/view_dispatcher.h>
 
-
+/**
+ * This structure tracks the main relevant information about the application.
+ */
 typedef struct {
     ViewDispatcher* view_dispatcher;
     //SceneManager* scene_manager;
-} InputHandling;
+} InputHandlingData;
 
-InputHandling* input_handling_alloc() {
-    InputHandling* instance = malloc(sizeof(InputHandling));
+/**
+ * This lists all available views allocated to our view dispatcher. 
+ */
+typedef enum {
+    InputHandlingViewDialog,
+    InputHandlingViewTextInput,
+    InputHandlingViewMAX
+} InputHandlingViews;
+
+/**
+ * Allocates the memory for an InputHandlingData
+ */
+InputHandlingData* input_handling_data_alloc() {
+    InputHandlingData* instance = malloc(sizeof(InputHandlingData));
     
     instance->view_dispatcher = view_dispatcher_alloc();
 
@@ -18,15 +32,21 @@ InputHandling* input_handling_alloc() {
     return instance;
 }
 
-void input_handling_free(InputHandling* instance) {
+/**
+ * Frees all data associated with this app. 
+ */
+void input_handling_data_free(InputHandlingData* instance) {
     view_dispatcher_free(instance->view_dispatcher);
 
     free(instance);
 }
 
+/**
+ * Entry point for our program. 
+ */
 int input_handling_app() {
-    InputHandling* app = input_handling_alloc();
+    InputHandlingData* app = input_handling_data_alloc();
 
-    input_handling_free(app);
+    input_handling_data_free(app);
     return 0;
 }
